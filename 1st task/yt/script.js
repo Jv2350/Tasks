3516906fd774e28f611b1357b453255135fc0a1b
Jv2350 <tp8375735@gmail.com>
2024-04-30 19:17:44 +0530

    new

---
+++ 1st task/yt/script.js
@@ -0,0 +1,9 @@
+console.log("hello")
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+})
