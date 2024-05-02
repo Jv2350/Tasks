69b481c10bba96327b602d8902b8ae9896b67dad
Jv2350 <tp8375735@gmail.com>
2024-05-02 21:58:14 +0530

    updated xo game

--- 3rd task/yt/script.js
+++ 3rd task/yt/script.js
@@ -31,33 +31,31 @@ let playerXIcon = "fas fa-times",
     runBot = true;
 
 
-// user click function
-function clickedBox(element) {
-    console.log(element);
+    // user click function
+    function clickedBox(element) {
     if (players.classList.contains("player")) {
-        element.innerHTML = `<i class="${playerOIcon}"></i>`;
-        players.classList.add("active");
         playerSign = "O";
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.remove("active");
         element.setAttribute("id", playerSign);
     } else {
         element.innerHTML = `<i class="${playerXIcon}"></i>`;
-        players.classList.add("active");
         element.setAttribute("id", playerSign);
+        players.classList.add("active");
     }
     selectWinner();
-    playBoard.style.pointerEvents = "none"
     element.style.pointerEvents = "none";
-    let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
-    setTimeout((runBot) => {
-        bot();
-    }, randomDelayTime);
+    playBoard.style.pointerEvents = "none";
+    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
+    setTimeout(() => {
+        bot(runBot);
+    }, randomTimeDelay);
 }
 
-
-function bot(runBot) {
+function bot() {
+    let array = [];
     if (runBot) {
         playerSign = "O";
-        let array = [];
         for (let i = 0; i < allBox.length; i++) {
             if (allBox[i].childElementCount == 0) {
                 array.push(i);
@@ -65,11 +63,11 @@ function bot(runBot) {
         }
         let randomBox = array[Math.floor(Math.random() * array.length)];
         if (array.length > 0) {
-            if (players.classList.contains("players")) {
-                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-                players.classList.remove("active");
+            if (players.classList.contains("player")) {
                 playerSign = "X";
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
                 allBox[randomBox].setAttribute("id", playerSign);
+                players.classList.add("active");
             } else {
                 allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
                 players.classList.remove("active");
@@ -82,35 +80,37 @@ function bot(runBot) {
         playerSign = "X";
     }
 }
-function getClass(idname) {
-    return document.querySelector(".box" + idname).id;
+
+function getIdVal(classname) {
+    return document.querySelector(".box" + classname).id;
 }
-function checkClass(val1, val2, val3, sign) {
-    if (getClass(val1) == sign && getClass(val2) == sign && getClass(val3) == sign) {
+function checkIdSign(val1, val2, val3, sign) {
+    if (getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign) {
         return true;
     }
 }
 function selectWinner() {
-    if (checkClass(1, 2, 3, playerSign) || checkClass(4, 5, 6, playerSign) || checkClass(7, 8, 9, playerSign) || checkClass(1, 4, 7, playerSign) || checkClass(2, 5, 8, playerSign) || checkClass(3, 6, 9, playerSign) || checkClass(1, 5, 9, playerSign) || checkClass(3, 5, 7, playerSign)) {
-        console.log(playerSign + " is the winner!!");
+    if (checkIdSign(1, 2, 3, playerSign) || checkIdSign(4, 5, 6, playerSign) || checkIdSign(7, 8, 9, playerSign) || checkIdSign(1, 4, 7, playerSign) || checkIdSign(2, 5, 8, playerSign) || checkIdSign(3, 6, 9, playerSign) || checkIdSign(1, 5, 9, playerSign) || checkIdSign(3, 5, 7, playerSign)) {
         runBot = false;
         bot(runBot);
         setTimeout(() => {
-            playBoard.classList.remove("show");
             resultBox.classList.add("show");
-        }, 700)
-        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!!`;
+            playBoard.classList.remove("show");
+        }, 700);
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
     } else {
-        if (getClass(1) != "" && getClass(2) != "" && getClass(3) != "" && getClass(4) != "" && getClass(5) != "" && getClass(6) != "" && getClass(7) != "" && getClass(8) != "" && getClass(9) != "") {
+        if (getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != "") {
+            runBot = false;
             bot(runBot);
             setTimeout(() => {
-                playBoard.classList.remove("show");
                 resultBox.classList.add("show");
-            }, 700)
-            wonText.textContent = `Match had been drawn!`;
+                playBoard.classList.remove("show");
+            }, 700);
+            wonText.textContent = "Match has been drawn!";
         }
     }
 }
+
 replayBtn.onclick = () => {
     window.location.reload();
 }
