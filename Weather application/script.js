f811c29b9614b415ba10523123caa108680700db
Jv2350 <tp8375735@gmail.com>
2024-05-17 01:57:42 +0530

    js updated

--- Weather application/script.js
+++ Weather application/script.js
@@ -1,14 +1,13 @@
 let cityInput = document.querySelector("#city_input");
-searchBtn = document.querySelector("#searchBtn");
-api_key = `80d0aa23fd84b45cf1ccc482926c292c`;
-currentWeatherCard = document.querySelectorAll(".weather-left .card")[0];
-fiveDaysForecastCard = document.querySelector(".day-forecast");
-
+let searchBtn = document.querySelector("#searchBtn");
+let api_key = `80d0aa23fd84b45cf1ccc482926c292c`;
+let currentWeatherCard = document.querySelectorAll(".weather-left .card")[0];
+let fiveDaysForecastCard = document.querySelector(".day-forecast");
 
 function getWeatherDetails(name, lat, lon, country, state) {
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
     let WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
-    days = [
+    let days = [
         'Sunday',
         'Monday',
         'Tuesday',
@@ -17,11 +16,11 @@ function getWeatherDetails(name, lat, lon, country, state) {
         'Friday',
         'Saturday',
     ];
-    months = [
+    let months = [
         'Jan',
-        'Fed',
+        'Feb',
         'Mar',
-        'Api',
+        'Apr',
         'May',
         'Jun',
         'Jul',
@@ -31,25 +30,26 @@ function getWeatherDetails(name, lat, lon, country, state) {
         'Nov',
         'Dec',
     ];
+
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
         let date = new Date();
         currentWeatherCard.innerHTML = `
             <div class="current-weather">
                 <div class="details">
-                <p>Now</p>
-                <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;C</h2>
-                <p>${data.weather[0].description}</p>
+                    <p>Now</p>
+                    <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;C</h2>
+                    <p>${data.weather[0].description}</p>
                 </div>
                 <div class="weather-icon">
-                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+                    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather icon">
                 </div>
             </div>
             <hr>
             <div class="card-footer">
-                <p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]} ${date.getFullYear()}</p>
+                <p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}</p>
                 <p><i class="fa-light fa-location-dot"></i> ${name}, ${country}</p>
             </div>
-        `
+        `;
     }).catch(() => {
         alert("Failed to fetch current weather");
     });
@@ -59,33 +59,38 @@ function getWeatherDetails(name, lat, lon, country, state) {
         let fiveDaysForecast = data.list.filter(forecast => {
             let forecastDate = new Date(forecast.dt_txt).getDate();
             if (!uniqueForecastDays.includes(forecastDate)) {
-                return uniqueForecastDays.push(forecast);
+                return uniqueForecastDays.push(forecastDate);
             }
         });
         fiveDaysForecastCard.innerHTML = '';
         for (let i = 1; i < fiveDaysForecast.length; i++) {
             let date = new Date(fiveDaysForecast[i].dt_txt);
             fiveDaysForecastCard.innerHTML += `
-            <div class="forecast-item">
-                <div class="icon-wrapper">
-                    <img src="https://openweathermap.org./img/wn/${fiveDaysForecast[i].weather[0].icon}.png" alt="">
-                    <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
+                <div class="forecast-item">
+                    <div class="icon-wrapper">
+                        <img src="https://openweathermap.org/img/wn/${fiveDaysForecast[i].weather[0].icon}.png" alt="weather icon">
+                        <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
+                    </div>
+                    <p>${date.getDate()} ${months[date.getMonth()]}</p>
+                    <p>${days[date.getDay()]}</p>
                 </div>
-                <p>${date.getDate()} ${months[date.getMonth()]}</p>
-                <p>${days[date.getDay()]}</p>
-            </div>
             `;
         }
     }).catch(() => {
-        alert("Failed to fetch wether forecast")
-    })
+        alert("Failed to fetch weather forecast");
+    });
 }
+
 function getCityCoordinates() {
     let cityName = cityInput.value.trim();
     cityInput.value = '';
     if (!cityName) return;
     let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${api_key}`;
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        if (data.length === 0) {
+            alert(`No coordinates found for ${cityName}`);
+            return;
+        }
         let { name, lat, lon, country, state } = data[0];
         getWeatherDetails(name, lat, lon, country, state);
     }).catch(() => {
